fe988928015e9c40a31127acfa5a87b504e4e555
Slach <bloodjazman@gmail.com>
2025-05-07 13:46:34 +0400

    Revert "fix: clean file paths to avoid duplicate temp dir segments"
    This reverts commit 162e2f337a609ead71fceef1b84b17ad869f175e.

--- storage/file.go
+++ storage/file.go
@@ -41,9 +41,7 @@ func (f *FileStorage) debugf(format string, args ...interface{}) {
 
 // Upload writes data to a local file
 func (f *FileStorage) Upload(filename string, reader io.Reader, format string, level int) error {
-	// Clean the filename path first to avoid duplicate path segments
-	cleanFilename := filepath.Clean(filename)
-	fullPath := filepath.Join(f.basePath, cleanFilename)
+	fullPath := filepath.Join(f.basePath, filename)
 	f.debugf("Uploading file: %s (compression: %s level %d)", fullPath, format, level)
 
 	// Ensure directory exists
