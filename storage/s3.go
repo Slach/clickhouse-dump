fc9d7af13fae134199bc0364dd90349fc1d50a05
Slach <bloodjazman@gmail.com>
2025-05-10 17:39:44 +0400

    code cleanup after aider

--- storage/s3.go
+++ storage/s3.go
@@ -10,10 +10,10 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	awsV2Logging "github.com/aws/smithy-go/logging"
 )
 
